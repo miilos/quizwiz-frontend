5526893a009102abcc02e7c7417def54b93cbbbc
Milos Popovic <milos.pop.004@gmail.com>
2026-01-08 23:07:12 +0100

    Add quiz search

--- src/components/QuizList/QuizList.tsx
+++ src/components/QuizList/QuizList.tsx
@@ -1,11 +1,20 @@
 import { gql } from "@apollo/client";
-import { useQuery } from "@apollo/client/react";
+import { useLazyQuery, useQuery } from "@apollo/client/react";
 import { Spinner } from "react-bootstrap";
 import Quiz from "../Quiz/Quiz";
 import type { Quiz as QuizType } from "../../types";
+import SearchBar from "../SearchBar/SearchBar";
 
 interface QuizResult {
   quizzes: [QuizType]
+}
+
+interface QuizSearchResult {
+  searchQuizzes: [QuizType]
+}
+
+interface SearchQuizInput {
+  keywords: string
 }
 
 const GET_ALL_QUIZZES = gql`
@@ -31,16 +40,51 @@ const GET_ALL_QUIZZES = gql`
   }
 `
 
+const SEARCH_QUIZZES = gql`
+  query SearchQuizzes($query: SearchQuizInput!) {
+    searchQuizzes(searchParams: $query) {
+      id
+      title
+      description
+      createdAt
+      tags {
+        id
+        name
+        displayName
+        
+      }
+      questions {
+        id
+      }
+      author {
+        id
+        username
+      }
+    }
+  }
+`
+
 function QuizList() {
   const { loading, error, data } = useQuery<QuizResult>(GET_ALL_QUIZZES)
+  const [ searchQuizzes, { loading: searchLoading, error: searchError, data: searchData } ] = useLazyQuery<QuizSearchResult>(SEARCH_QUIZZES)
 
-  if (loading) {
+  const handleSearch = (query: SearchQuizInput) => {
+    searchQuizzes({
+      variables: { query }
+    })
+  }
+
+  const isLoading = searchLoading ?? loading
+  const hasError = searchError ?? error
+  const quizData = searchData?.searchQuizzes ?? data?.quizzes
+
+  if (isLoading) {
     return (
       <Spinner animation="border" role="status" className="loader loader--primary" />
     )
   }
 
-  if (error) {
+  if (hasError) {
     return (
       <div className="error"></div>
     )
@@ -48,13 +92,21 @@ function QuizList() {
 
   return (
     <>
+      <SearchBar handleSearch={handleSearch} />
+
       <div className="quiz-list-container">
         <h1 className="quiz-list__title">Browse quizzes</h1>
 
         <div className="quiz-list__quizzes">
-          { data?.quizzes.map(
-            quiz => <Quiz quiz={quiz} key={quiz.id} />
-          ) }
+          { 
+            quizData?.length ? (
+              quizData?.map(
+                quiz => <Quiz quiz={quiz} key={quiz.id} />
+              )
+            ) : (
+              <p>There are no search results for these keywords :(</p>
+            )
+          }
         </div>
       </div>
     </>
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,38 @@
-function SearchBar() {
+import React, { useState } from "react"
+import { IoSearch } from "react-icons/io5"
+
+interface SearchBarProps {
+  handleSearch: CallableFunction
+}
+
+function SearchBar({ handleSearch }: SearchBarProps) {
+  const [query, setQuery] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    handleSearch({
+      keywords: query
+    })
+    setQuery('')
+  }
+
   return (
     <>
+      <form className="searchbar" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="searchbar__input"
+          placeholder="Search quizzes..."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="searchbar__btn">
+          <IoSearch />
+        </button>
+      </form>
     </>
   )
 }
